496092626c2f1518e0e4b6859ca01f657813780e
FIZA ASLAM <chfizu190@gmail.com>
2026-05-02 00:23:14 +0500

    Refactor app.py to include sklearn import

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import pickle
-
+import sklearn          
+import sklearn.ensemble  
 # ============================================
 # PAGE SETUP
 # ============================================
